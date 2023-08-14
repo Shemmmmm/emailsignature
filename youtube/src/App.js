e5d04feb7091d04063552b5251bc5e6cc90a6e53
Shem Nyachoti <83941045+Shemmmmm@users.noreply.github.com>
2023-08-14 21:55:35 +0000

    adding search bar

--- youtube/src/App.js
+++ youtube/src/App.js
@@ -3,6 +3,10 @@ import './App.css';
 function App() {
   return (
     <div className="App">
+      <div>
+        <input type='text'/>
+        <input type='button'><img src='https://www.creativefabrica.com/wp-content/uploads/2019/02/Search-icon-by-Kanggraphic-1.jpg' alt= 'search icon'/></input>
+      </div>
       <section role='navigation'>
         <div>
           <img src='https://t3.ftcdn.net/jpg/04/03/98/64/360_F_403986499_hB7zfgOXezReA0sKkxl34RoT9TbNkbpH.jpg' alt='youtubeicon'/>
@@ -34,8 +38,6 @@ function App() {
           <span id='span8'>Cars</span>
         </div>
       </section>
-      
-      
     </div>
   );
 }
