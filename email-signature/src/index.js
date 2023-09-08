ca607c4d6267d26a774530f084d0a9cf3efcd973
Shem Nyachoti <83941045+Shemmmmm@users.noreply.github.com>
2023-09-08 13:34:29 +0000

    modified:   email-signature/src/index.js

--- email-signature/src/index.js
+++ email-signature/src/index.js
@@ -1,11 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
-import {BrowserRouter,Routes,Route} from "react-router-dom";
-import Home from "./components/Home";
-import About from "./components/About";
-  
+import reportWebVitals from './reportWebVitals';  
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
