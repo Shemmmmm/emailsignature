7b934689926a5c0228cfd07a274b1c4f05eff781
Shem Nyachoti <83941045+Shemmmmm@users.noreply.github.com>
2023-09-08 13:35:35 +0000

    modified:   email-signature/src/App.js

--- email-signature/src/App.js
+++ email-signature/src/App.js
@@ -1,6 +1,9 @@
 import Search from './components /search';
 import './App.css';
-
+import {BrowserRouter,Routes,Route} from "react-router-dom";
+import Home from "./components/Home";
+import About from "./components/About";
+  
 function App() {
   return (
     <Search />
