679b0fd49ed0f1d58ff72a18ac4d45021b26e269
Shem Nyachoti <83941045+Shemmmmm@users.noreply.github.com>
2023-09-08 13:47:35 +0000

    modified:   email-signature/src/App.js

--- email-signature/src/App.js
+++ email-signature/src/App.js
@@ -1,12 +1,14 @@
 import Search from './components /search';
 import './App.css';
 import {BrowserRouter,Routes,Route} from "react-router-dom";
-import Home from "./components/Home";
-import About from "./components/About";
   
 function App() {
   return (
-    <Search />
+      <BrowserRouter>
+         <Routes>
+             <Route exact path="/components/search" element={<Search/>}/>
+           </Routes>
+     </BrowserRouter>
   );
 }
 
