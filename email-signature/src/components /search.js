14b0751fa27bbaf3516943ff416de606fbe35850
Shem Nyachoti <83941045+Shemmmmm@users.noreply.github.com>
2023-09-10 14:00:43 +0000

    deleted:    email-signature/public/manifest.json

--- email-signature/src/components /search.js
+++
@@ -1,8 +0,0 @@
-import { useState } from "react"; 
-
-export default function Search(){
-    const [text, setText] = useState('');
-    return(
-        <input type='text' value={text} onChange={setText(text)} />
-    );
-}
--- email-signature/src/components /signin.js
+++
@@ -1,7 +0,0 @@
-export default function Signin(params) {
-   handleClick(){
-   }
-    return (
-        <button onClick= {handleClick} >Sign in</button>
-    );
-}
