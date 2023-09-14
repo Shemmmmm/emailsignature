98c59e5bfd52f1bae0a86fe43f010f059e52ce4d
Shem Nyachoti <83941045+Shemmmmm@users.noreply.github.com>
2023-09-14 17:10:43 +0000

    modified:   youtube/pages/index.js 	modified:   youtube/pages/index.js

--- youtube/pages/index.js
+++ youtube/pages/index.js
@@ -8,7 +8,7 @@ import {useState} from 'react';
 const inter = Inter({ subsets: ['latin'] })
 
 export default function Home() {
-  const fileRef = ref(storage, 'Videos/screen-20230810-060148.mp4');
+  const fileRef = ref(storage);// 'Videos/screen-20230810-060148.mp4');
   const [videoUrl, setVideoUrl] = useState('');
   fileRef.getDownloadURL().then((url) => {
     
