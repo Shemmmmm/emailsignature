fdb6838c79e5be5e49235d7eff20f55358553df2
Shem Nyachoti <83941045+Shemmmmm@users.noreply.github.com>
2023-09-20 22:04:59 +0000

    modified:   youtube/pages/api/hello.js

--- youtube/pages/api/hello.js
+++
@@ -1,6 +0,0 @@
-// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
-
-export default function handler(req, res) {
- const url = fetch('gs://fir-c5188.appspot.com/VID-20230916-WA0002.mp4');
- return url;
-}
