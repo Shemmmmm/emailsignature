bc5cacd327bce8eb5938d39f9af6ff4abc8c4aa4
Shem Nyachoti <83941045+Shemmmmm@users.noreply.github.com>
2023-09-20 20:52:33 +0000

    modified:   youtube/firebase /firebase/config/firebase.js

--- youtube/firebase /firebase/config/firebase.js
+++ youtube/firebase /firebase/config/firebase.js
@@ -2,23 +2,24 @@
 import { initializeApp } from "firebase/app";
 import { getAnalytics } from "firebase/analytics";
 import { getStorage } from "firebase/storage";
+import * as firebase from 'firebase/app';
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: process.env.NODE_ENV.API_KEY,
-  authDomain: process.env.NODE_ENV.AUTH_DOMAIN,
-  projectId: process.env.NODE_ENV.PROJECT_ID,
-  storageBucket: process.env.NODE_ENV.STORAGE_BUCKET,
-  messagingSenderId: process.env.NODE_ENV.MESSAGING_SENDER_ID,
-  appId: process.env.NODE_ENV.APP_ID,
-  measurementId: process.env.NODE_ENV.MEASUREMENT_ID  
+  apiKey: process.env.API_KEY,
+  authDomain: process.env.AUTH_DOMAIN,
+  projectId: process.env.PROJECT_ID,
+  storageBucket: process.env.STORAGE_BUCKET,
+  messagingSenderId: process.env.MESSAGING_SENDER_ID,
+  appId: process.env.APP_ID,
+  measurementId: process.env.MEASUREMENT_ID  
 };
 
               // Initialize Firebase
-              const app = initializeApp(firebaseConfig);
-               const storage = getStorage(app);
+      const storage = !firebase.getApps.length   ?  initializeApp(firebaseConfig):
+                getStorage(initializeApp(firebaseConfig));
               // const analytics = getAnalytics(app)
             export default storage;
