ea69cd1b869da06b538243a673fc25d0ac47c7c8
Shem Nyachoti <83941045+Shemmmmm@users.noreply.github.com>
2023-10-03 16:09:12 +0000

    modified:   youtube/pages/index.js

--- youtube/pages/index.js
+++ youtube/pages/index.js
@@ -3,9 +3,9 @@ import Head from 'next/head'
 // import { Inter } from 'next/font/google'
 // const inter = Inter({ subsets: ['latin'] }) '@/components/displayVideos';
 export default function Home() {
-const [url, setUrl] = useState('');
+const [url, setUrl] = useState(null);
 () =>
-fetch('https://ap-south-1.aws.data.mongodb-api.com/app/data-deeyw/endpoint/Cluster0/YouTube/Videos').then((data) =>{ setUrl(data)});
+fetch('https://ap-south-1.aws.data.mongodb-api.com/app/data-deeyw/Cluster0/YouTube/Videos').then((data) =>{ setUrl(data)});
   return (
     <>
       <Head>
@@ -15,8 +15,8 @@ fetch('https://ap-south-1.aws.data.mongodb-api.com/app/data-deeyw/endpoint/Clust
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <main>
-        <video src={url} width={300+'px'} height ={300+ 'px'} controls ></video>
-        {url.length}
+        <video src="https://player.vimeo.com/external/553898838.sd.mp4?s=8ca77c64bc418983dcbce830b1458b911e4b7c6b&profile_id=164&oauth2_token_id=57447761" width={300+'px'} height ={300+ 'px'} controls ></video>
+        
       </main>
       </>
       )
