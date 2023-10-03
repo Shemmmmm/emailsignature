c91ca7156c9e731e331c68984d215c8e9f87ef93
Shem Nyachoti <83941045+Shemmmmm@users.noreply.github.com>
2023-10-03 20:27:55 +0000

    modified:   youtube/pages/api/videos.js

--- youtube/pages/api/videos.js
+++ youtube/pages/api/videos.js
@@ -5,14 +5,12 @@ export default async function handler(req, res) {
   const client = await clientPromise;
     const db = client.db("nextjs-mongodb-demo");
       switch (req.method) {
-          case "POST":
-                let bodyObject = JSON.parse(req.body);
-                      let myPost = await db.collection("posts").insertOne(bodyObject);
-                            res.json(myPost.ops[0]);
-                                  break;
-                                      case "GET":
-                                            const allPosts = await db.collection("allPosts").find({}).toArray();
-                                                  res.json({ status: 200, data: allPosts });
-                                                        break;
-                                                          }
-                                                          }
+             case "POST":
+   let bodyObject = JSON.parse(req.body);
+         let myPost = await db.collection("Videos").insertOne(bodyObject);
+res.json(myPost.ops[0]);
+    break;
+       case "GET":
+         const allPosts = await db.collection("Videos").find({}).toArray(); res.json({ status: 200,    data : Videos } );
+         break;
+          }  }
