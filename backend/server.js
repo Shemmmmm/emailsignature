4737a2bbb4eb3fe6f1dce3bd2463bf46db6aa01f
Shem Nyachoti <83941045+Shemmmmm@users.noreply.github.com>
2023-12-17 09:48:07 +0000

    new file:   backend/models/User.js 	new file:   backend/server.js

---
+++ backend/server.js
@@ -0,0 +1,32 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const User = require('./models/User');
+
+const app = express();
+const PORT = process.env.PORT || 5000;
+
+mongoose.connect('mongodb://localhost:27017/your-database-name', {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+
+app.get('/api/users', async (req, res) => {
+  try {
+    const users = await User.find();
+    res.json(users);
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ message: 'Server Error' });
+  }
+});
+
+app.post('/signup', async(req, res) => {
+    const { email, firstName } = req.body
+    const user = new User({ email, firstName })
+    const ret = await user.save();
+    res.json(ret);
+  });
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
