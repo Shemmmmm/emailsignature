80d096db379399029c1f722bad31d8adf67bf1f6
Shem Nyachoti <83941045+Shemmmmm@users.noreply.github.com>
2023-12-17 09:02:27 +0000

    modified:   youtube/src/index.css

--- youtube/src/App.js
+++ youtube/src/App.js
@@ -1,9 +1,9 @@
 import './App.css';
-import {Login} from "./components/login.js";
+import {SignUp} from "./components/signup.js";
 function App() {
   return (
     <div className="App">
-      <Login/>
+      <SignUp/>
     </div>
   );
 }
